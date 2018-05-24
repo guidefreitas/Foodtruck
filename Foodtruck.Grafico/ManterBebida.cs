396ca2228e94af9df64efafb84114bf05df92382
Mahlimas <37126557+Mahlimas@users.noreply.github.com>
2018-05-24 19:40:42 -0300

    Adicao da tela bebidas e seus metodos
    Adicionada a tela bebidas e as funcoes: adicionar, remover e alterar

---
+++ Foodtruck.Grafico/ManterBebida.cs
@@ -0,0 +1,72 @@
+﻿using Foodtruck.Negocio;
+using Foodtruck.Negocio.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Foodtruck.Grafico
+{
+    public partial class ManterBebida : Form
+    {
+        public Bebida BebidaSelecionada { get; set; }
+
+        public ManterBebida()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            Bebida bebida = new Bebida();
+
+            bebida.Id = Convert.ToInt64(tbId.Text);
+            bebida.Nome = tbNome.Text;
+            bebida.Valor = Convert.ToDecimal(tbValor.Text);
+            bebida.Tamanho = Convert.ToSingle(tbTamanho.Text);
+
+            Validacao validacao;
+            if (BebidaSelecionada == null)
+            {
+                validacao = Program.Gerenciador.CadastraBebida(bebida);
+            }
+            else
+            {
+                validacao = Program.Gerenciador.AlterarBebida(bebida);
+            }
+
+            if (!validacao.Valido)
+            {
+                String mensagemValidacao = "";
+                foreach (var chave in validacao.Mensagens.Keys)
+                {
+                    String msg = validacao.Mensagens[chave];
+                    mensagemValidacao += msg;
+                    mensagemValidacao += Environment.NewLine;
+                }
+                MessageBox.Show(mensagemValidacao);
+            }
+            else
+            {
+                MessageBox.Show("Bebida salva com sucesso!");
+                this.Close();
+            }
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ManterBebida_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
